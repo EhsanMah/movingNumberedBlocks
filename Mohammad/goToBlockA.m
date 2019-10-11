b6bdda56bac375022e10e1ed41e8d03b28dc7b90
Mohammadrastilari <54830123+Mohammadrastilari@users.noreply.github.com>
2019-10-11 21:45:25 +1100

    update

--- Mohammad/goToBlockA.m
+++ Mohammad/goToBlockA.m
@@ -1,10 +1,12 @@
 function [] = goToBlockA()
 cartsvc_ = rossvcclient('/dobot_magician/PTP/set_cartesian_pos');
 cartmsg_ = rosmessage(cartsvc_);
-% defaultGrid();
+
 cartmsg_.TargetPoints=[0.284,0.05,0.018,0];
 cartsvc_.call(cartmsg_)
+
 suctionOn();
+
 cartmsg_.TargetPoints=[0.284,0.043,-0.036,0];    
 cartsvc_.call(cartmsg_)
 suctionOff();
